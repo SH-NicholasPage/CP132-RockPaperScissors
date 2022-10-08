1303b919aa5233db3aba5acfac4cbee75aa1f1c8
SH-NicholasPage <npage@southhills.edu>
2022-10-08 19:46:31 -0400

    Removed nullable

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -2,8 +2,6 @@
 * Name: [YOUR NAME HERE]
 * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
 */
-#pragma warning disable CS8600
-#pragma warning disable CS8602 
 using System;
 
 namespace RockPaperScissors
